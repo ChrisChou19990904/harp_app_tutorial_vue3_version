fa3624f6fc3197fe29a2a27ab1c40cade85e6055
Chris Chou <D1397102@o365.fcu.edu.tw>
2025-12-05 05:08:58 +0800

    初始化專案

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/harp_app_tutorial_vue3_version/',
   plugins: [
     vue(),
     vueDevTools(),
