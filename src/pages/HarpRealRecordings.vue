66183f382cad29002d057a7b85eb4829df5d84ed
Chris Chou <D1397102@o365.fcu.edu.tw>
2025-12-22 10:07:26 +0800

    asd

--- src/pages/HarpRealRecordings.vue
+++ src/pages/HarpRealRecordings.vue
@@ -1,11 +1,14 @@
 <template>
   <PageWrapper>
     <ol>
+      <li>光的戰役<br><video controls><source src="/光的戰役.mp4" type="video/mp4"></video></li>
       <li>帝皇再臨<br><video controls><source src="/帝皇再臨.mp4" type="video/mp4"></video></li>
       <li>Just KO<br><video controls><source src="/Just_KO.mp4" type="video/mp4"></video></li>
+      <li>Miracle of Ultra<br><video controls><source src="/miracle_of_ultra.mp4" type="video/mp4"></video></li>
       <li>中華民國軍樂-下課號<br><video controls><source src="/中華民國軍樂-下課號.mp4" type="video/mp4"></video></li>
       <li>穿越時空的思念<br><video controls><source src="/穿越時空的思念.mp4" type="video/mp4"></video></li>
-      <li><router-link to="/">一眉道人</router-link><br><video controls><source src="/一眉道人.mp4" type="video/mp4"></video></li>
+      <li>哀歌<br><video controls><source src="/elegy.mp4" type="video/mp4"></video></li>
+      <li>一眉道人<br><video controls><source src="/一眉道人.mp4" type="video/mp4"></video></li>
       <li>殭屍復活II<br><video controls><source src="/殭屍復活II.mp4" type="video/mp4"></video></li>
       <li>Samsung Galaxy Alarm - Homecoming<br><video controls><source src="/homecoming.mp4" type="video/mp4"></video></li>
       <li>益可膚<br><video controls><source src="/益可膚.mp4" type="video/mp4"></video></li>
